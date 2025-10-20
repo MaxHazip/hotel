35c3387a3bd8a3d31e8ae27a9b1b70e6dfb2f6b4
Студент группы IS <is@sielom.local>
2025-10-20 10:58:02 +0500

    error date

--- hotel_project/hotel_app/views.py
+++ hotel_project/hotel_app/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
-from datetime import datetime
+from datetime import datetime, timedelta
 
 # Create your views here.
 def main(request):
 
     today = datetime.now()
+    tomorrow = today + timedelta(days=1)
+    today_str = today.strftime('%m.%d.%Y')
+    tomorrow_str = tomorrow.strftime('%m.%d.%Y')
 
     context = {
-        'today': today,
+        'today': today_str,
+        'tomorrow': tomorrow_str,
     }
 
     return render(request, 'main_content.html', context)
